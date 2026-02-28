6ff4c8904b86db97c01bf21ce6a98e9281926361
Quang Tran <qdtran.dev@gmail.com>
2026-02-28 17:19:00 -0500

    Sync recipes to main frame for proper display

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -3,13 +3,11 @@ class Recipe {
   final String imagePath;
   final List<String> ingredients;
   final String instructions;
-  bool isFavorite;      // grad feature
 
   Recipe({
     required this.name,
     required this.imagePath,
     required this.ingredients,
     required this.instructions,
-    this.isFavorite = false,
   });
 }
